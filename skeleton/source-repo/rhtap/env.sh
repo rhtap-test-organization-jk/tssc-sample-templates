abe9993aaeab06a873d10f8ef554a4bc6fb779ef
John Duimovich <jduimovich@gmail.com>
2024-07-04 15:12:28 -0400

    jenkins-$TAG

--- skeleton/source-repo/rhtap/env.sh
+++ skeleton/source-repo/rhtap/env.sh
@@ -2,8 +2,9 @@
 export REBUILD=true
 export SKIP_CHECKS=true 
 
-# from buildah-rhtap 
-export IMAGE_URL=${{ values.image }} 
+# from buildah-rhtap
+TAG=$(date +%s)
+export IMAGE_URL=${{ values.image }}:jenkins-$TAG
 export IMAGE=$IMAGE_URL
 export RESULT_PATH=$DIR/results/temp/files/sbom-url
 #export XDG_RUNTIME_DIR=/home/john/dev/auth-creds
